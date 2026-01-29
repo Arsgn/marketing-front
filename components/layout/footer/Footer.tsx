d8a456cff5e480492fbce3614cb28fc690269eb7
Arsgn <blackbarber1323@gmail.com>
2026-01-29 17:17:33 +0600

    header-footer

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,13 +1,52 @@
-'use client'
-import { FC } from 'react';
-import scss from './Footer.module.scss';
+"use client";
+import { FC } from "react";
+import scss from "./Footer.module.scss";
 
 export const Footer: FC = () => {
-    return (
-        <footer className={scss.Footer}>
-            <div className='container'>
-                <div className={scss.content}>Footer</div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={scss.Footer}>
+      <div className="container">
+        <div className={scss.content}>
+          <div className={scss.block}>
+            <h1>Logo</h1>
+            <p className={scss.text}>
+              (Название)— это частная виртуальная сеть с уникальными функциями и
+              высоким уровнем безопасности.
+            </p>
+           <div className={scss.top}>
+            <button>.</button>
+            <button>.</button>
+            <button>.</button>
+           </div>
+            <p className={scss.Link}>©2020LaslesVPN</p>
+          </div>
+          <div className={scss.box}>
+          <ul>
+            <li>Продукт</li>
+            <li>Download </li>
+            <li>Pricing</li>
+            <li>Locations</li>
+            <li>Server</li>
+            <li>Countries</li>
+            <li>Blog</li>
+          </ul>
+          <ul>
+            <li>Engage</li>
+            <li>LaslesVPN ?</li>
+            <li>FAQ</li>
+            <li>Tutorials</li>
+            <li>About Us</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+          </ul>
+          <ul>
+            <li>Earn Money</li>
+            <li>Affiliate</li>
+            <li>Become Partner</li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,13 +1,26 @@
-'use client'
-import { FC } from 'react';
-import scss from './Header.module.scss';
+"use client";
+import { FC } from "react";
+import scss from "./Header.module.scss";
+import Link from "next/link";
 
 export const Header: FC = () => {
-    return (
-        <header className={scss.Header}>
-            <div className='container'>
-                <div className={scss.content}>Header</div>
-            </div>
-        </header>
-    );
+  return (
+    <header className={scss.Header}>
+      <div className="container">
+        <div className={scss.content}>
+          <h2>Logo</h2>
+          <nav>
+            <Link href="/">Главная</Link>
+            <Link href="">О нас</Link>
+            <Link href="">Курсы</Link>
+            <Link href="">Контакты</Link>
+          </nav>
+          <div className={scss.Buttons}>
+          <button className={scss.SignIn}>Войти</button>
+          <button className={scss.Join}>Присоединяйся</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
